0788dff474df2598631c0b50e2ee4e9671fe6f42
Manny Becerra <39503964+mannybecerra@users.noreply.github.com>
2024-12-06 12:03:16 -0800

    partial enablement of user allowed route

--- app/(chat)/api/user/profile/[id]/allowed/route.ts
+++ app/(chat)/api/user/profile/[id]/allowed/route.ts
@@ -1,39 +1,39 @@
-// import { NextResponse, type NextRequest } from 'next/server';
 // import { getUserAllowedDb } from '@/actions/user-db';
-// import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
+import { type NextRequest, NextResponse } from 'next/server';
 
-// import type { USER_MODEL } from '@/types/user';
+import type { USER_MODEL } from '@/types/user';
 
-// // https://beta.nextjs.org/docs/routing/route-handlers
+// https://beta.nextjs.org/docs/routing/route-handlers
 
-// // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#runtime
-// // export const runtime = 'nodejs';
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#runtime
+// export const runtime = 'nodejs';
 
-// export { postHandler as POST };
+export { postHandler as POST };
 
-// type UserSaveParamTypes = {
-//   userId: USER_MODEL['id'];
-// };
+type UserSaveParamTypes = {
+  userId: USER_MODEL['id'];
+};
 
-// // https://beta.nextjs.org/docs/routing/route-handlers#dynamic-route-handlers
-// const postHandler = async (
-//   request: NextRequest,
-//   { params }: { params: { id: UserSaveParamTypes['userId'] } }
-// ) => {
-//   const userId = params.id;
+// https://beta.nextjs.org/docs/routing/route-handlers#dynamic-route-handlers
+const postHandler = async (
+  request: NextRequest,
+  { params }: { params: { id: UserSaveParamTypes['userId'] } },
+) => {
+  const userId = params.id;
 
-//   if (!userId) {
-//     const statusText = 'User ID not provided';
-//     return new Response(statusText, {
-//       status: StatusCodes.BAD_REQUEST,
-//       statusText,
-//     });
-//   }
+  if (!userId) {
+    const statusText = 'User ID not provided';
+    return new Response(statusText, {
+      status: StatusCodes.BAD_REQUEST,
+      statusText,
+    });
+  }
 
-//   const isUserAllowed = await getUserAllowedDb(userId);
+  //   const isUserAllowed = await getUserAllowedDb(userId);
 
-//   // 2. All done!
-//   return NextResponse.json({
-//     allowed: isUserAllowed,
-//   });
-// };
+  // 2. All done!
+  return NextResponse.json({
+    // allowed: isUserAllowed,
+  });
+};
