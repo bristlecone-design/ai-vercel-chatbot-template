cb71ec533a65fe924c3b7bb937f73179f902a07b
Manny Becerra <39503964+mannybecerra@users.noreply.github.com>
2025-01-04 06:21:18 -0800

    key

--- components/overview.tsx
+++ components/overview.tsx
@@ -34,6 +34,7 @@ function OverviewAvatar({
 
   const renderedAvatar = (
     <motion.div
+      key={userAvatar}
       initial={{ opacity: 0, scale: 0.9 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.98 }}
